0e8a4e78020e7de7bf1f5d129d34d8b2a6e42972
Eli0uce <eliouce.gaming@outlook.fr>
2022-11-29 15:00:05 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button:
+let mybutton = document.getElementById("btt");
+
+// Scroll to the top of the page on click
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
